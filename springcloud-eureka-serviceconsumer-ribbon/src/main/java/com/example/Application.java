a0779f5ab2e4fdda45c52336db984fde670184c4
hzliubenlong <hzliubenlong@corp.netease.com>
2019-02-09 19:07:01 +0800

    springcloud+eureka+ribbon

---
+++ springcloud-eureka-serviceconsumer-ribbon/src/main/java/com/example/Application.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient  //启用服务发现组件
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * springcloud中提倡rest风格的微服务
+     * 想spring容器中注入RestTemplate
+     *
+     * 使用LoadBalanced表明这个restRemplate开启负载均衡的功能。
+     * @return
+     */
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ springcloud-eureka-serviceprovider/src/main/java/com/example/Application.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+
+    @RequestMapping("/")
+    public String hi(String name) {
+        return String.format("hello %s , from port=%s", name, port);
+    }
+}
