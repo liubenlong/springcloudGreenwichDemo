26233dd24179e1423a9f09b5687f306a35c549b6
hzliubenlong <hzliubenlong@corp.netease.com>
2019-02-09 20:49:20 +0800

    ribbon + hystrix

--- springcloud-eureka-serviceconsumer-ribbon/src/main/java/com/example/Application.java
+++ springcloud-eureka-serviceconsumer-ribbon/src/main/java/com/example/Application.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient //启用EurekaClient组件
 @EnableDiscoveryClient  //启用服务发现组件
+@EnableHystrix //启用Hystrix
 public class Application {
 
     public static void main(String[] args) {
--- springcloud-eureka-serviceconsumer-ribbon/src/main/java/com/example/HelloController.java
+++ springcloud-eureka-serviceconsumer-ribbon/src/main/java/com/example/HelloController.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,15 @@ public class HelloController {
     RestTemplate restTemplate;
 
     @RequestMapping("/")
+    @HystrixCommand(fallbackMethod = "hiFallBack")
     public String hi(String name){
         //这里直接写的是服务名： springcloud-eureka-serviceprovider  。在ribbon中它会根据服务名来选择具体的服务实例，根据服务实例在请求的时候会用具体的url替换掉服务名
         return restTemplate.getForObject("http://springcloud-eureka-serviceprovider?name=" + name, String.class);
     }
+
+    public String hiFallBack(String name) {
+        return " fall back. " + name;
+    }
+
+
 }
