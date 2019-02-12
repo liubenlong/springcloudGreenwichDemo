514e2d81e02a73349bc0eebe3c627ede25626df2
hzliubenlong <hzliubenlong@corp.netease.com>
2019-02-12 11:50:11 +0800

    springcloudconfig

--- springcloud-config-client/src/main/java/com/example/Application.java
+++ springcloud-config-client/src/main/java/com/example/Application.java
@@ -1,24 +1,14 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Value("${name}")
-    String name;
 
-    @RequestMapping(value = "/hi")
-    public String hi(){
-        return "hi "+name;
-    }
 }
