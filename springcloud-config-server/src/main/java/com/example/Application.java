decbac56fa43d20aa6a878267ec06896040dc3e3
hzliubenlong <hzliubenlong@corp.netease.com>
2019-02-13 11:21:53 +0800

    springcloudconfig

--- springcloud-config-server/src/main/java/com/example/Application.java
+++ springcloud-config-server/src/main/java/com/example/Application.java
@@ -3,9 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer //启用spring cloud config 服务
+@EnableEurekaClient //启用EurekaClient服务
 public class Application {
 
     public static void main(String[] args) {
